39091a26b2d572cd37ac94c005fec6b9880b7b9b
unknown <5dpixel56@gmail.com>
2024-11-17 17:26:43 +0000

    Tried to add images a few hours ago but doesn't work yet

--- catalystengine-docs/src/app/app.routes.ts
+++ catalystengine-docs/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from "./intro/intro.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'intro', component: IntroComponent }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
